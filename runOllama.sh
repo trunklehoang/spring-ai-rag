b8b36f851f4526972d0925a92b9328d03273762f
Trunk Le <mohoatu@gmail.com>
2024-03-05 11:32:50 +0700

    initial project skeleton

---
+++ runOllama.sh
@@ -0,0 +1,12 @@
+#docker run -d --gpus=all -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
+docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
+docker start ollama
+#docker stop ollama
+#docker exec -it ollama ollama run orca2:13b
+docker exec -it ollama ollama run stable-beluga:latest
+#docker exec -it ollama ollama run falcon:40b
+#docker exec -it ollama ollama run sqlcoder:15b
+#docker exec -it ollama ollama run llama2
+
+
+#docker exec -it ollama bash
