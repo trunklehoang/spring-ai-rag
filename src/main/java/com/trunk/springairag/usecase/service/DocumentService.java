1d101c1f4b8d03dcde232157646a1687476f521d
Trunk Le <mohoatu@gmail.com>
2024-03-14 16:45:35 +0700

    improve prompt

--- src/main/java/com/trunk/springairag/usecase/service/DocumentService.java
+++ src/main/java/com/trunk/springairag/usecase/service/DocumentService.java
@@ -23,10 +23,22 @@ public class DocumentService {
     private final ChatClient chatClient;
 
     private static final String PROMPT = """ 
-           from + {ocr} +
-           provide the date and total amount in the json format
-            """;
-
+             Use the following pieces of information to answer the user's question.
+                 If you don't know the answer, just say that you don't know, don't try to make up an answer.
+                        
+                 Context: {context}
+                 Question: from + {ocr} + retrieve two value the date and total amount in the json format
+                 Only return the helpful answer below and nothing else.
+                 Helpful answer:
+             """;
+    private static final String PROMPT2 ="""
+<|system|>Using the information contained in the context,
+give a comprehensive answer to the question.
+If the answer is contained in the context, also report the source URL.
+If the answer cannot be deduced from the context, do not give an answer.</s>
+<|user|>
+  Question:from + {ocr} + retrieve two value the date and total amount in the json format
+""";
     public Message getSystemMessage(String prompt) throws IOException {
         Resource resource = new ClassPathResource("ocr/" + prompt + ".json"); // Adjust folder path as needed
         String ocr = "";
@@ -43,7 +55,7 @@ public class DocumentService {
         } else {
             throw new IOException("File not found: " + prompt);
         }
-        SystemPromptTemplate systemPromptTemplate =  new SystemPromptTemplate(PROMPT);
+        SystemPromptTemplate systemPromptTemplate =  new SystemPromptTemplate(PROMPT2);
         Prompt promptCommand = new Prompt(List.of(systemPromptTemplate.createMessage(Map.of("ocr", ocr))));
         ChatResponse response = chatClient.call(promptCommand);
         return response.getResult().getOutput();
