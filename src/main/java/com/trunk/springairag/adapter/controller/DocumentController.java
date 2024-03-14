1cfae21a65d0730025c904523b6daf78a6ffa048
Trunk Le <mohoatu@gmail.com>
2024-03-14 14:53:08 +0700

    initial code

--- src/main/java/com/trunk/springairag/adapter/controller/DocumentController.java
+++ src/main/java/com/trunk/springairag/adapter/controller/DocumentController.java
@@ -7,13 +7,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("rest/document")
 @AllArgsConstructor
 public class DocumentController {
     private final DocumentService documentService;
     @GetMapping("/search/{prompt}")
-    public String postDocumentSearch(@PathVariable("prompt") String prompt) {
+    public String postDocumentSearch(@PathVariable("prompt") String prompt) throws IOException {
         return this.documentService.getSystemMessage(prompt).getContent();
     }
 }
--- src/main/java/com/trunk/springairag/usecase/service/DocumentService.java
+++ src/main/java/com/trunk/springairag/usecase/service/DocumentService.java
@@ -6,8 +6,14 @@ import org.springframework.ai.chat.ChatResponse;
 import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.SystemPromptTemplate;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 
@@ -17,14 +23,28 @@ public class DocumentService {
     private final ChatClient chatClient;
 
     private static final String PROMPT = """ 
-			You're assisting with questions about documents in a catalog.
-			Use the information from the DOCUMENTS section to provide accurate answers.
-			If unsure, simply state that you don't know." + " {prompt}"
-			""";
+           from + {ocr} +
+           provide the date and total amount in the json format
+            """;
 
-    public Message getSystemMessage(String prompt) {
+    public Message getSystemMessage(String prompt) throws IOException {
+        Resource resource = new ClassPathResource("ocr/" + prompt + ".json"); // Adjust folder path as needed
+        String ocr = "";
+        // Check if the resource exists
+        if (resource.exists()) {
+            try (InputStream inputStream = resource.getInputStream()) {
+                // Read the file content into a byte array
+                byte[] fileBytes = FileCopyUtils.copyToByteArray(inputStream);
+                // Convert byte array to String using UTF-8 encoding
+                ocr = new String(fileBytes, StandardCharsets.UTF_8);
+            } catch (IOException e) {
+                throw new IOException("have some error: " + e.getMessage(), e);
+            }
+        } else {
+            throw new IOException("File not found: " + prompt);
+        }
         SystemPromptTemplate systemPromptTemplate =  new SystemPromptTemplate(PROMPT);
-        Prompt promptCommand = new Prompt(List.of(systemPromptTemplate.createMessage(Map.of("prompt", prompt))));
+        Prompt promptCommand = new Prompt(List.of(systemPromptTemplate.createMessage(Map.of("ocr", ocr))));
         ChatResponse response = chatClient.call(promptCommand);
         return response.getResult().getOutput();
     }
