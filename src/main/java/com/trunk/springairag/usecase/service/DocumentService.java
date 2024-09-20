2217067c21a3e70e7ec9b690a0d380c191dccdd7
trunk <tlehoangquang@openwt.com>
2024-09-20 17:47:24 +0700

    change to category

--- src/main/java/com/trunk/springairag/usecase/service/DocumentService.java
+++ src/main/java/com/trunk/springairag/usecase/service/DocumentService.java
@@ -19,12 +19,21 @@ import java.util.Map;
 @AllArgsConstructor
 public class DocumentService {
     private static final String TEXT = """
-              Task: extract the date and total amount from the text.
-              Instructions:
-              1. summary the date and total amount from the extracted text in the Context and return JSON object with two field : date & total_amount
-              2. If not recognized, return an empty JSON object.
-            Context:{ocr}
-              """;
+        Task: Extract the date and total amount from the OCR text and categorize the expense.
+        Instructions:
+        1. Summarize the extracted date and total amount from the provided OCR text in the Context.
+            Analyze the extracted text for keywords or phrases related to the following categories:
+            a. Travelling: Look for keywords like "taxi," "flight," "car rental," "bus," "airport."
+            b. Lodging and Living: Look for keywords like "hotel," "accommodation," "stay," "rental."
+            c. Telecom: Look for keywords like "phone," "internet," "mobile," "data," "SIM."
+            d. IT Equipment: Look for keywords like "laptop," "tablet," "hardware," "keyboard."
+            e. Train Ticket: Look for keywords like "train," "rail," "ticket," "station.","Terminal-ID"
+            f. Plane: Look for "flight," "boarding pass," "airfare," "airport."
+            g. Meals: Look for keywords like "food," "restaurant," "dining," "meal," "breakfast," "lunch," "dinner."
+            h. Team Events: Look for "team," "event," "outing," "celebration."
+        2. If not recognized, return an empty JSON object.
+        Context:{ocr}
+        """;
     public String getSystemMessage(String prompt) throws IOException {
         Resource resource = new ClassPathResource("ocr/" + prompt + ".json"); // Adjust folder path as needed
         String ocr;
